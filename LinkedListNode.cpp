67f64b6d03be0011411f62875d73a396ffd3965b
nguyentiendu <45115358+nguyentiendu@users.noreply.github.com>
2019-05-22 11:49:53 +0700

    Add files via upload

---
+++ LinkedListNode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "SLinkedList.h"
+using namespace std;
+template<typename E>
+int main()
+{
+    SLinkedList<E> x;
+    /*x.addFront("BOS");
+    x.addFront("ATL");
+    x.addFront("MSP");
+    x.addFront("LAX");
+    x.print();
+    x.removeFront();
+    x.print();*/
+    return 0;
+}
